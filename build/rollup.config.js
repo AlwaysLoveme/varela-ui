88ec4d36d2e879b3d6cd5609a1a8b2d96bb4f406
codingman <zhuxian@SKATTING-4249.local>
2021-07-21 17:54:36 +0800

    update package.json

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -12,14 +12,14 @@ export default {
     {
       format: "iife", // IIFE
       file: resolvePath("../lib/index.browser.js"),
-      name: "ui",
+      name: "varela",
       globals,
       exports: "named",
     },
     {
       format: "umd", // umd格式
       file: resolvePath("../lib/index.umd.js"), // 输出文件
-      name: "new-ui", // 指定name
+      name: "varela", // 指定name
       exports: "named",
       globals,
       assetFileNames: "assets/[name].[hash][extname]",
